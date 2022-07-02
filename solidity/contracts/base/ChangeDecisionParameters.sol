5c379ff1f2f6161555fd332b3263086866c78e57
Toon Macharis <Toon.Macharis@gmail.com>
2022-07-02 17:43:38 +0200

    reduce the compiled contract size a bit by using 1 extended struct instead of the original struct and another value.

--- solidity/contracts/base/ChangeDecisionParameters.sol
+++ solidity/contracts/base/ChangeDecisionParameters.sol
@@ -4,13 +4,24 @@ pragma solidity ^0.8.9;
 
 import 'contracts/base/Proposals.sol';
 
+struct ExtendedDecisionParameters {
+    uint64 decisionTime; //How much time in seconds shareholders have to approve a request
+    uint64 executionTime; //How much time in seconds the owner has to execute an approved request after the decisionTime has ended
+    //to approve a vote, both the quorum and the majority need to be reached.
+    //a vote is approved if and only if the quorum and the majority are reached on the decisionTime, otherwise it is rejected
+    uint32 quorumNumerator;     //the required quorum is calculated as quorumNumerator/quorumDenominator
+    uint32 quorumDenominator;   //the required quorum is compared to the number of votes that are in favor, against or abstain divided by the total number of votes
+    uint32 majorityNumerator;   //the required majority is calculated as majorityNumerator/majorityDenominator and must be greater than 1/2
+    uint32 majorityDenominator; //the required majority is compared to the number of votes that are in favor divided by the number of votes that are either in favor or against
+    uint16 voteType;
+}
+
 abstract contract ChangeDecisionParameters is Proposals {
-    mapping(uint256 => uint16) private decisionParametersVoteType;
-    mapping(uint256 => DecisionParameters) private decisionParametersData;
+    mapping(uint256 => ExtendedDecisionParameters) private decisionParametersData;
 
     function getProposedDecisionParameters(uint256 id) external view virtual override returns (uint16, uint64, uint64, uint32, uint32, uint32, uint32) {
-        DecisionParameters storage dP = decisionParametersData[id];
-        return (decisionParametersVoteType[id], dP.decisionTime, dP.executionTime, dP.quorumNumerator, dP.quorumDenominator, dP.majorityNumerator, dP.majorityDenominator);
+        ExtendedDecisionParameters storage eDP = decisionParametersData[id];
+        return (eDP.voteType, eDP.decisionTime, eDP.executionTime, eDP.quorumNumerator, eDP.quorumDenominator, eDP.majorityNumerator, eDP.majorityDenominator);
     }
 
     function changeDecisionParameters(ActionType voteType, uint64 decisionTime, uint64 executionTime, uint32 quorumNumerator, uint32 quorumDenominator, uint32 majorityNumerator, uint32 majorityDenominator) external virtual override returns (uint256) {
@@ -28,29 +39,28 @@ abstract contract ChangeDecisionParameters is Proposals {
 
         uint256 id = getNextProposalId(); //lambdas are planned, but not supported yet by Solidity, so initialization has to happen outside the propose method
 
-        decisionParametersVoteType[id] = voteType;
-
-        DecisionParameters storage dPD = decisionParametersData[id];
-        dPD.decisionTime = decisionTime;
-        dPD.executionTime = executionTime;
-        dPD.quorumNumerator = quorumNumerator;
-        dPD.quorumDenominator = quorumDenominator;
-        dPD.majorityNumerator = majorityNumerator;
-        dPD.majorityDenominator = majorityDenominator;
+        ExtendedDecisionParameters storage eDP = decisionParametersData[id];
+        eDP.voteType = voteType;
+        eDP.decisionTime = decisionTime;
+        eDP.executionTime = executionTime;
+        eDP.quorumNumerator = quorumNumerator;
+        eDP.quorumDenominator = quorumDenominator;
+        eDP.majorityNumerator = majorityNumerator;
+        eDP.majorityDenominator = majorityDenominator;
 
         return propose(uint16(ActionType.CHANGE_DECISION_PARAMETERS), executeSetDecisionParameters, requestSetDecisionParameters);
     }
 
     function executeSetDecisionParameters(uint256 id, VoteResult voteResult) internal returns (bool) {
-        uint16 voteType = decisionParametersVoteType[id];
 
-        DecisionParameters storage dPD = decisionParametersData[id];
-        uint64 decisionTime = dPD.decisionTime;
-        uint64 executionTime = dPD.executionTime;
-        uint32 quorumNumerator = dPD.quorumNumerator;
-        uint32 quorumDenominator = dPD.quorumDenominator;
-        uint32 majorityNumerator = dPD.majorityNumerator;
-        uint32 majorityDenominator = dPD.majorityDenominator;
+        ExtendedDecisionParameters storage eDP = decisionParametersData[id];
+        uint16 voteType = eDP.voteType;
+        uint64 decisionTime = eDP.decisionTime;
+        uint64 executionTime = eDP.executionTime;
+        uint32 quorumNumerator = eDP.quorumNumerator;
+        uint32 quorumDenominator = eDP.quorumDenominator;
+        uint32 majorityNumerator = eDP.majorityNumerator;
+        uint32 majorityDenominator = eDP.majorityDenominator;
         
         if (isApproved(voteResult)) {
             DecisionParameters storage dP = decisionParameters[voteType];
@@ -68,8 +78,8 @@ abstract contract ChangeDecisionParameters is Proposals {
     }
 
     function requestSetDecisionParameters(uint256 id) internal {
-        DecisionParameters storage dP = decisionParametersData[id];
+        ExtendedDecisionParameters storage eDP = decisionParametersData[id];
 
-        emit RequestChangeDecisionParameters(id, decisionParametersVoteType[id], dP.decisionTime, dP.executionTime, dP.quorumNumerator, dP.quorumDenominator, dP.majorityNumerator, dP.majorityDenominator);
+        emit RequestChangeDecisionParameters(id, eDP.voteType, eDP.decisionTime, eDP.executionTime, eDP.quorumNumerator, eDP.quorumDenominator, eDP.majorityNumerator, eDP.majorityDenominator);
     }
 }
