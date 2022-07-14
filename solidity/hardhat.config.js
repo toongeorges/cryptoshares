28f6a56d56b207a581777f42a6ce46edaddd7a43
Toon Macharis <Toon.Macharis@gmail.com>
2022-07-14 05:18:14 +0200

    remove web3 dependency, because ethers will be used instead

--- solidity/hardhat.config.js
+++ solidity/hardhat.config.js
@@ -27,11 +27,3 @@ module.exports = {
     }
   }
 };
-
-extendEnvironment((hre) => {
-  const Web3 = require("web3");
-  hre.Web3 = Web3;
-
-  // hre.network.provider is an EIP1193-compatible provider.
-  hre.web3 = new Web3(hre.network.provider);
-});
